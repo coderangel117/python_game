d633b532e51f19604b0ed44f2b850b47dfbad777
Gabriel perino <perino_g@etna-alternance.net>
2022-12-29 17:08:17 +0100

    refactor[mystery_number] :  - Improve loop game  - Add condition in game function

--- guess_number.py
+++ guess_number.py
@@ -2,24 +2,29 @@ import random
 
 
 def main():
-    test = input('I am a test')
-    print(test)
-    pass
+    choice = -1
+    choice = input(f" Que voulez vous faire\n"
+                   f"0 - Quitter\n"
+                   f"1 - Jouer\n")
+    while choice != '0':
+        user_choice = int(choice)
+        if user_choice == 1:
+            guess_number()
+    exit(1)
 
 
 def guess_number():
     number_min: int = 1
     number_max: int = 100
-    random_number: int = random.randint(number_min, number_max)
-    user_number = input(f'Entrez un nombre entre {number_min} et {number_max}')
-
-    while random_number:
-        pass
-
-    if user_number > number_max or user_number < number_min:
+    foo = random.randint(number_min, number_max)
+    print(foo)
+    user_number = input(f'Entrez un nombre entre {number_min} et {number_max}\n')
+    user_number = int(user_number)
+    if user_number == foo:
+        print('Congrats')
+        exit()
+    elif user_number >= number_max or user_number <= number_min:
         print('tou tou fout de ma gueule ?!')
-    else:
-        pass
     return number_max
 
 
