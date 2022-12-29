51e1369a09f3ea04b747a68b22c67a58aa99e8fc
Gabriel PERINO <perino_g@etna-alternance.net>
2022-12-29 14:23:34 +0100

    feat[concatenate] : - Add file and create test function - Add .idea folder in gitignore

---
+++ concatenate.py
@@ -0,0 +1,11 @@
+# just create function who concatenate user input if user write something
+def myprint(input):
+    if input == "":
+        print('No input')
+    else:
+        print(f"You have write {input}")  # concatenate with f string method
+
+
+if __name__ == '__main__':
+    inputuser = input('enter something and press enter key \n')  # ask user to write something in console
+    myprint(inputuser)  # use function defined before
