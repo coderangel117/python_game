ca7a3a3d7bf9cbe397cdadaee2a3a7fe587bc1bb
Gabriel PERINO <perino_g@etna-alternance.net>
2022-12-31 01:52:33 +0100

    feat[mystery_number] : structure game - Add replay menu - Add user_manager menu - Add loop function to improve code in main function - Add function's docstring

--- mystery_number.py
+++ mystery_number.py
@@ -1,18 +1,53 @@
 import random
+import user_manager
 
 
-# @TODO ==> add project CASINO in notion (with multiples games (chosen in a menu))
-# @TODO ==> add project hyrule castle in python in notion (check if possible before)
-# @TODO ==> Add in this project username and nb won / lose game in a table (disply ina function statistics)
-def display_menu():
+def main_menu():
+    """
+    Display main menu
+    :return: int: return user's answer
+    """
+    answer = 0
+    while answer != 1 and answer != 2 and answer != 3:
+        answer = int(input(
+            """
+            [1] - Start the game   
+            [2] - Manage users
+            [3] - Exit the game
+            """))
+    return answer
+
+
+def replay_menu():
+    """
+    Display replay menu after game
+    :return: int : return user's answer
+    """
     answer = 0
     while answer != 1 and answer != 2:
-        answer = int(input('''
-        [1] - Start the game   
-        [2] - Exit the game
-    
-        '''))
+        answer = int(input(
+            '''voulez vous rejouer ?
+            [1] - Yes, let's go !
+            [2] - No, exit please'''))
     return answer
+
+
+def users_menu():
+    """
+    Display users' manager menu
+    :return: int
+    """
+    manage_choice = 0
+    while manage_choice != 1 and manage_choice != 2 and manage_choice != 3 and manage_choice != 4 and manage_choice != 5 and manage_choice != 6:
+        manage_choice = int(input('''
+        [1] - Display users list    
+        [2] - Search a specific user    
+        [3] - Create a new user
+        [4] - Update user info
+        [5] - Delete a user
+        [6] - return to main menu
+        '''))
+    return manage_choice
 
 
 def mystery_number():
@@ -60,27 +95,44 @@ def replay_menu():
 
 
 def check_win(table):
+    """
+    Check and return true if user wins
+    increment nb fail or nbwin user's property if fails or wins
+    :param table:
+    :return: boolean
+    """
     if table[0] == 1:
         print(f"You won with {table[1]} attempts")
         return True
     else:
         print('You loose because you doesn\'t find the number before the last attempt')
         return False
+    elif table[0] == -2:
+        print('You loose because you are a monkey')
+        return False
+
+
+def loop_replay(choice: int):
+    while choice != 3:
+        if int(choice) == 1:
+            table = mystery_number()
+            check_win(table)
+            choice = replay_menu()
+        else:
+            exit()
 
 
 def main():
-    user_choice = int(display_menu())
+    user_choice = int(main_menu())
     if user_choice == 1:
         return_table = mystery_number()
         check_win(return_table)
         choice_replay = replay_menu()
-        while choice_replay == 1:
-            if int(choice_replay) == 1:
-                table = mystery_number()
-                check_win(table)
-                choice_replay = replay_menu()
-            else:
-                exit()
+        loop_replay(choice_replay)
+    elif user_choice == 2:
+        choice = users_menu()
+        user_manager.loop_usermanager(choice)
+
     else:
         print("Au revoir et à bientôt")
         exit()
