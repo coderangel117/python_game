a28f798900522ed12d538de5e1ac6a724bb28f97
gab <perinogabriel@gmail.com>
2024-09-13 07:47:16 +0200

    feat [main] : tic_tac_toe - Add some comments - Add tic_tac_toe in menu - Add warning if user choose invite

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import json
 
 import mystery_number
 import rock_paper_scissors
+import tic_tac_toe
 import user_manager
 import utils
 
 
 #   @TODO Permit to play in another languages (fr and en)
+#   @TODO Permit to add a new user in game menu
+#   @TODO Compile a ranking of the best scores
 #   @TODO Add different level (easy, normal, hard) (default => normal but can be changed)
 #   @TODO Add a great score list per game (the same for fails and wins)
-# Possible games list = ["Pendu alias hang_man", "Roulette", "bingo", "Tic-Tac-Toe", "snake"]
-
-
+# possible_games_list = ["Pendu alias hang_man", "Roulette", "bingo", "Tic-Tac-Toe", "snake"]
 def choose_player():
     """
-        Permit to user to choose with wich profile he wants to play
+        user can choose with wich profile he wants to play
         :return: str
     """
     users = user_manager.get_user_files()
@@ -33,11 +34,12 @@ def choose_player():
             print("No user found in list... \n"
                   "User invite (default) selected ")
             user_manager.new_user('invite')
-            return 'invite'
+            return 'invite'  # if no user exists a default user is created and selected to play
         player = input('Which player do you want to play with ? \n')
         player_exists = user_manager.get_user_files().__contains__(player + '.json')
-        if player == "":
-            print(f"Great ! You play as invite ")
+        if player == "":  # Select the default user (invite) ( don't count in user's stat)
+            print("Great ! You play as invite \n"
+                  "WARNING: The game won't count in user's stat ")
             return "invite"
         else:
             while not player_exists:
@@ -56,11 +58,12 @@ def games_menu():
         :return: int
     """
     manage_choice = 0
-    while manage_choice != 1 and manage_choice != 2 and manage_choice != 3:
+    while manage_choice != 1 and manage_choice != 2 and manage_choice != 3 and manage_choice != 4:
         manage_choice = input('''
             [1] - Mystery number
             [2] - Rock paper scissors
-            [3] - return to main menu
+            [3] - Tic Tac Toe
+            [4] - return to main menu
             ''')
         if not utils.check_special_characters(manage_choice):
             manage_choice = 0
@@ -73,6 +76,11 @@ def games_menu():
         player = choose_player()
         result = rock_paper_scissors.rock_paper_scissors(player)
         check_win(result)
+    if manage_choice == 3:
+        player = choose_player()
+        result = tic_tac_toe.tic_tac_toe(player)
+        check_win(result)
+
     if manage_choice == 3:
         return "main"
     return manage_choice
@@ -87,14 +95,14 @@ def check_win(game_result: []):
     """
     user_manager.add_played_game(game_result[2])
     if game_result[0] == 1:
-        if game_result[3] == "shifoumi":
+        if game_result[3] == "mystery_number":
             print(f"You won with {game_result[1]} attempts")
-        else:
+        elif game_result[3] == "shifoumi":
             print(f"You won with {game_result[1]} points")
         user_manager.add_win(game_result[2])
         return True
     elif game_result[0] == -1:
-        print('You loose because you doesn\'t find the number before the last attempt')
+        print("You loose because you doesn't find the number before the last attempt")
         user_manager.add_fail(game_result[2])
         return False
     elif game_result[0] == -2:
